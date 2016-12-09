a1bf68eb3e995aff0f48f28e2c286f7851e8aab8
thanhdatvo <thanhdat.vo.email@gmail.com>
2016-12-09 23:45:53 +0700

    f

---
+++ BackEnd/BookOnline/Models/IOrderManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookOnline.Models
+{
+    public interface IOrderManager
+    {
+        IEnumerable<Order> GetAll();
+        Order Get(int id);
+        Order Add(Order order);
+        void Remove(int id);
+        bool Update(Order order);
+    }
+}
