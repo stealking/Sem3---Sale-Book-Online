3a6acfa4a2e37c0c06d10e89f220283f3f0589cc
thanhdatvo <thanhdat.vo.email@gmail.com>
2016-12-09 23:13:37 +0700

    new

---
+++ BackEnd/BookOnline/Models/IUserManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookOnline.Models
+{
+        public interface IUserManager
+    {
+        IEnumerable<User> GetAll();
+        User Get(int id);
+        User Add(User user);
+        void Remove(int id);
+        bool Update(User user);
+    }
+}
